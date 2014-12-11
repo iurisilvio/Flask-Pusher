6d25d665badf0db73457f4f60faa2ed48dc06bc2
Iuri de Silvio <iurisilvio@gmail.com>
2014-12-11 10:10:17 -0200

    Changelog and version bump to 0.3

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+Changelog
+=========
+0.3
+ * Webhooks support
+ * JSONP authentication support with Flask-Jsonpify
+
+0.2
+---
+ * Authentication support
+
+0.1
+---
+ * Initial version
+ * Flask app binding to pusher client
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Pusher',
-    version='0.3dev',
+    version='0.3',
     url='https://www.github.com/iurisilvio/Flask-Pusher',
     license='MIT',
     author='Iuri de Silvio',
